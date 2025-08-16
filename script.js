a4f96e4df96930f39444b5ef208f66d4452fdff3
Soukaina Chkirid <soukainayoussefchkirid@gmail.com>
2025-08-16 12:29:29 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById('heart').addEventListener('click', function() {
+  const messageContainer = document.getElementById('message-container');
+  messageContainer.classList.toggle('show-message');
+});
